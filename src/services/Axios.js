372125abe5d596cf07eda5608924eda2de7763a1
Дмитрий Кравец <gmankrz@gmail.com>
2022-10-28 12:33:41 +0300

    Second Test Commit
    * просто изменён временный Жадресс сервера в Axios.js

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // const REACT_APP_API_BASE = "http://localhost:7000/"; https://77d7-178-155-4-156.eu.ngrok.io
-const REACT_APP_API_BASE = "https://8346-37-145-166-1.eu.ngrok.io/";
+const REACT_APP_API_BASE = "https://63dd-37-145-167-3.eu.ngrok.io/";
 const $host = axios.create({
     baseURL: REACT_APP_API_BASE,
 });
