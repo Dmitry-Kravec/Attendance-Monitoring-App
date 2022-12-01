dae156d0456d3761b05beaba4cef763281ef65b0
Дмитрий Кравец <gmankrz@gmail.com>
2022-12-01 10:23:00 +0300

    qr-style update

--- src/styles/student-qr-scanner-style.js
+++ src/styles/student-qr-scanner-style.js
@@ -4,15 +4,14 @@ const styles = StyleSheet.create({
     container: {
         flexGrow: 1,
         alignItems: "center",
-        justifyContent: "flex-start",
+        justifyContent: "center",
     },
     barCodeBox: {
-        marginTop: 100,
+        marginTop: "5%",
         alignItems: "center",
         justifyContent: "center",
-        height: 300,
+        height: "60%",
         width: 300,
-        overflow: "hidden",
     },
     permissionContainer: {
         display: "flex",
@@ -24,12 +23,12 @@ const styles = StyleSheet.create({
         textAlign: "center",
         marginBottom: 30,
     },
-    barCodeScanner: { height: 600, width: 500 },
+    barCodeScanner: { height: "100%", width: 300 },
     buttonView: {
-        marginTop: 40,
+        marginTop: "6%",
     },
     buttonViewText: {
-        marginBottom: 30,
+        marginBottom: "5%",
     },
     buttonViewInside: {
         display: "flex",
